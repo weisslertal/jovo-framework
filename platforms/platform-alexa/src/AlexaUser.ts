2118cf8589f4161f32d4525093f71dadd877006e
Tal Weissler <weisslertal@gmail.com>
2022-06-01 15:11:01 +0300

    Revert ":sparkles: Add get profile properties functions"
    This reverts commit 524a23a0057a9b1942b3bfbaa4df6b6667aded90.

--- platforms/platform-alexa/src/AlexaUser.ts
+++ platforms/platform-alexa/src/AlexaUser.ts
@@ -29,29 +29,13 @@ export class AlexaUser extends JovoUser<Alexa> {
   }
 
   async getEmail(): Promise<string | undefined> {
-    return await this.getProfilePropery(ProfileProperty.EMAIL) as string;
-  }
-
-  async getMobileNumber(): Promise<{ countryCode: string; mobileNumber: string; } | undefined> {
-    return await this.getProfilePropery(ProfileProperty.MOBILE_NUMBER) as { countryCode: string; mobileNumber: string; };
-
-  }
-
-  async getName(): Promise<string | undefined> {
-    return await this.getProfilePropery(ProfileProperty.NAME) as string;
-  }
-
-  async getGivenName(): Promise<string | undefined> {
-    return await this.getProfilePropery(ProfileProperty.GIVEN_NAME) as string;
-  }
-
-  private async getProfilePropery(property: ProfileProperty): Promise<unknown> {
     const request: AlexaRequest = this.jovo.$request;
-    return sendCustomerProfileApiRequest(
-      property,
+    const email: string = await sendCustomerProfileApiRequest(
+      ProfileProperty.EMAIL,
       request.getApiEndpoint(),
       request.getApiAccessToken(),
     );
+    return email;
   }
 
   async setReminder(reminder: AbsoluteReminder | RelativeReminder): Promise<ReminderResponse> {
